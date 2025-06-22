f32c83033acbde158243911310f2c01a56b010df
perkinson1251 <tttrashutin@gmail.com>
2025-06-23 00:41:57 +0300

    feat: create basic config and generic .env file

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '../../', '');
+
+  return {
+    plugins: [react()],
+    envDir: '../../',
+    server: {
+      port: Number(env.FRONTEND_PORT),
+    },
+  };
 });
