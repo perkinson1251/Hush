d4098e984dbb05602183381bff1d1ecf58f93ec9
perkinson1251 <tttrashutin@gmail.com>
2025-06-24 00:38:55 +0300

    chore(server): change console.log to logger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,18 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const configService = app.get(ConfigService);
   const port = configService.get<number>('BACKEND_PORT') || 3000;
 
-  app.setGlobalPrefix('api');
+  const logger = new Logger('Bootstrap');
 
+  app.setGlobalPrefix('api');
   await app.listen(port);
-  console.log(`Application is running on port ${port}`);
-  console.log(`API available at: http://localhost:${port}/api`);
+
+  logger.log(`Application is running on port ${port}`);
+  logger.log(`API available at: http://localhost:${port}/api`);
 }
 
 void bootstrap();
