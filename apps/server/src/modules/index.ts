161c29a85786fe006c321493c9c54f249457a3c2
perkinson1251 <tttrashutin@gmail.com>
2025-06-24 00:37:35 +0300

    feat(server): add basic chat socket

--- apps/server/src/modules/index.ts
+++ apps/server/src/modules/index.ts
@@ -1,4 +1,8 @@
 import { ModuleMetadata } from '@nestjs/common';
+import { ChatModule } from './chat/chat.module';
 import { PingModule } from './ping/ping.module';
 
-export const MODULES: NonNullable<ModuleMetadata['imports']> = [PingModule];
+export const MODULES: NonNullable<ModuleMetadata['imports']> = [
+  PingModule,
+  ChatModule,
+];
